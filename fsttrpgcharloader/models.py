b1bae7be229f96a1c35073be91ad30c1f1f3f7ab
Krypticdator <toni.nurmi@hotmail.com>
2016-06-23 21:51:12 +0300

    added Codacy Shield

--- fsttrpgcharloader/models.py
+++ fsttrpgcharloader/models.py
@@ -1,23 +1,35 @@
 import aws
+from __future__ import print_function
 class JsonListOfActors(object):
     def __init__(self):
         super(JsonListOfActors, self).__init__()
         self.actors = dict()
 
-    def load(self, actor_role):
+    def load(self, actor_role, with_raws=False):
         json = aws.get_aws_character_list(actor_role)
         for actor in json:
             name = str(actor['name'])
             attributes = dict()
+            raws = dict()
             for key, value in actor.iteritems():
                 attributes[str(key)] = str(value)
+                if with_raws:
+                    raws[key] = value
+            if with_raws:
+                attributes['raws'] = raws
             self.actors[name] = attributes
 
 
-        #print(str(self.actors))
 
     def get_name_list(self):
         names = []
         for key, value in self.actors.iteritems():
             names.append(key)
         return names
+
+    def get_optional_value(self, actor,  valuename):
+        try:
+            return self.actors[actor][valuename]
+        except KeyError as e:
+            print('no such key as ' + valuename)
+            return None
