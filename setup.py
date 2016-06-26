2febeaf5a033338fad844cca89150f184e629e5f
Krypticdator <toni.nurmi@hotmail.com>
2016-06-26 08:55:40 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FSTTRPGCharacterLoader',
-    version='0.1.3',
+    version='0.1.4',
     packages=['fsttrpgcharloader'],
     url='',
     license='',
